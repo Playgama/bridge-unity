75f85a198e4a1b38badde11f464f64129ef5fc12
Sergei <sergei@playgama.com>
2026-07-08 14:59:49 +0300

    Update JS Core

--- Runtime/WebGLTemplates/Bridge/playgama-bridge-unity.js
+++ Runtime/WebGLTemplates/Bridge/playgama-bridge-unity.js
@@ -111,7 +111,7 @@ function addLocalBridge() {
 }
 
 bridgeScript = document.createElement('script')
-bridgeScript.src = 'https://bridge.playgama.com/v1/stable/playgama-bridge.js'
+bridgeScript.src = 'https://bridge.playgama.com/v2/stable/playgama-bridge.js'
 bridgeScript.onload = initializeBridge
 bridgeScript.onerror = addLocalBridge
 
